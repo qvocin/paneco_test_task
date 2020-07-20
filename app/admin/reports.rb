e2009c9809f911e24d8b4c87ba07a01ffbe072ec
Viacheslav Smolianinov <qvocins@gmail.com>
2020-07-20 23:04:00 +0300

    add report query older than

--- app/admin/reports.rb
+++ app/admin/reports.rb
@@ -3,12 +3,21 @@
 ActiveAdmin.register_page 'Reports' do
   menu priority: 4
 
-  page_action :top_10_report, method: :get do
-    @customers = TopTenCustomersQuery.new.all
+  content title: 'Reports' do
+    div class: 'blank_slate_container', id: 'dashboard_default_message' do
+      span class: 'blank_slate' do
+        # span I18n.t('active_admin.dashboard_welcome.welcome')
+        # small I18n.t('active_admin.dashboard_welcome.call_to_action')
+      end
+    end
+  end
+
+  page_action :top_10_report, method: :post do
+    @customers = TopTenCustomersQuery.new(params['report_top_10']).top_10
     render admin_reports_top_10_report_path
   end
 
-  action_item :add do
-    link_to 'Make top 10 report', admin_reports_top_10_report_path, method: :get
+  sidebar :index do
+    render partial: 'admin/reports/form'
   end
 end
--- app/queries/top_ten_customers_query.rb
+++ app/queries/top_ten_customers_query.rb
@@ -2,14 +2,15 @@
 # rubocop:disable Metrics/AbcSize
 
 class TopTenCustomersQuery
-  attr_accessor :customers, :orders
+  attr_accessor :customers, :orders, :age
 
-  def initialize
+  def initialize(data)
     @customers = Customer.arel_table
     @orders = Order.arel_table
+    @age = data[:age].to_i
   end
 
-  def all
+  def top_10
     arel_query = build_arel_query
     ActiveRecord::Base.connection.execute(arel_query.to_sql)
   end
@@ -18,11 +19,17 @@ class TopTenCustomersQuery
 
   def build_arel_query
     customers.
-      project(customers[:email],
+      project(customers[:email], customers[:date_of_birth],
               Arel::Nodes::NamedFunction.new('SUM', [orders[:order_total]]).as('order_total_sum')).
       join(orders).on(customers[:id].eq(orders[:customer_id])).
-      where(customers[:id].eq(orders[:customer_id])).
-      group(customers[:email]).
+      where(where_conditions).
+      group(customers[:email], customers[:date_of_birth]).
       order('order_total_sum DESC').take(10)
+  end
+
+  def where_conditions
+    query = customers[:id].eq(orders[:customer_id])
+    query = query.and(customers[:date_of_birth].lteq(@age.years.ago)) if @age.present? && @age.positive?
+    query
   end
 end
